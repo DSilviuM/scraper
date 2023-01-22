3f8730ae4e8a01a37c06fd521ded18d14babc60c
DSilviuM <silviu.m.dobos@gmail.com>
2023-01-22 21:15:36 +0200

    test1

---
+++ test_1.js
@@ -0,0 +1,20 @@
+webSite='https://www.apavital.ro/intreruperi-de-apa'
+//webSite='https://the-internet.herokuapp.com/'
+
+var stringAllWebpage=""
+async function getText(file) {    
+    let myObject = await fetch(file)
+    let myText = await myObject.text()
+    stringAllWebpage = stringAllWebpage + myText.replace(/\r?\n/)  
+    console.log(stringAllWebpage) 
+    return stringAllWebpage
+    
+}
+console.log(stringAllWebpage.length)
+
+let theString=getText(webSite)
+
+console.log("theString ",theString)
+
+
+console.log("done")
