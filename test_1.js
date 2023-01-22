ffff78347cc6da34411da3e8926771773ac402b7
DSilviuM <silviu.m.dobos@gmail.com>
2023-01-22 22:04:46 +0200

    aa

--- test_1.js
+++ test_1.js
@@ -1,22 +1,25 @@
 webSite='https://www.apavital.ro/intreruperi-de-apa'
 //webSite='https://the-internet.herokuapp.com/'
 
-var stringAllWebpage=""
 async function getText(file) {    
     let myObject = await fetch(file)
     let myText = await myObject.text()
-    stringAllWebpage = stringAllWebpage + myText.replace(/\r?\n/)  
-    console.log(stringAllWebpage) 
+    stringAllWebpage = myText.replace(/\r?\n/)  
+    console.log(stringAllWebpage)
     return stringAllWebpage
     
 }
-console.log(stringAllWebpage.length)
+//let theString=getText(webSite)
 
-let theString=getText(webSite)
+async function secondFunction(){
+    await getText(webSite)
+    
+  }
 
-console.log("theString ",theString)
+secondFunction()
+console.log("done")
 
 // azi vreo jumatate de zi m-am chinuit sa pot folosi "stringAllWebpage" dupa ce este executata functia
 // dar problema este ca fiind o functie asyncron ea se executa intr-un thread separat iar codul meu nu asteapta ca ea sa fie gata
-// in seara asta mai stau un pic sa vad daca gasesc si o alta varianta ... :(, deci nu prea ai ce vdea ca am ramas cam blocat pana rezolv asta
-console.log("done")
+// in seara asta mai stau un pic sa vad daca gasesc si o alta varianta ... :(, deci nu prea ai ce vedea ca am ramas cam blocat pana rezolv asta
+
