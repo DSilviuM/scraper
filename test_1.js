59131881398ed0cb83a74b7baf13d14b7b936783
DSilviuM <silviu.m.dobos@gmail.com>
2023-01-22 21:29:57 +0200

    doar comments

--- test_1.js
+++ test_1.js
@@ -16,5 +16,7 @@ let theString=getText(webSite)
 
 console.log("theString ",theString)
 
-// test de update
+// azi vreo jumatate de zi m-am chinuit sa pot folosi "stringAllWebpage" dupa ce este executata functia
+// dar problema este ca fiind o functie asyncron ea se executa intr-un thread separat iar codul meu nu asteapta ca ea sa fie gata
+// in seara asta mai stau un pic sa vad daca gasesc si o alta varianta ... :(, deci nu prea ai ce vdea ca am ramas cam blocat pana rezolv asta
 console.log("done")
