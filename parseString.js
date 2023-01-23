34f1a845dd0f63a0fdaf252ec7f08fcdbb2d9571
DSilviuM <silviu.m.dobos@gmail.com>
2023-01-23 17:53:28 +0200

    test with small string

--- parseString.js
+++ parseString.js
@@ -5,3 +5,26 @@
 // Daca e string atunci e doar string
 
 
+let myString='<div class="btn-header"><este_2 href="tel:0232969"><span>Call center: 0232 969</span></a></div>'
+//let myString='<div class="btn-header"><este_1 <este_2 href="tel:0232969">><span>Call center: 0232 969</span></a></div>'
+
+let j=0
+let k=0
+
+for (let i = 0; i <= myString.length; i++) {
+    if (myString[i] == "<"){
+        j=i
+        
+    }else if(myString[i] == ">"){
+        k=i+1
+        if (k>j){
+            let result = myString.substring(j, k)
+            console.log(result)
+        }
+    }
+
+
+
+
+    //console.log(myString[i])
+  }
