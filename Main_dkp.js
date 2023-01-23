80069b70fd3a530e224fdeed75a9367f79af230b
DSilviuM <silviu.m.dobos@gmail.com>
2023-01-23 17:05:37 +0200

    first

---
+++ Main_dkp.js
@@ -0,0 +1,16 @@
+import {test} from './stringAllWebpage_dkp.js'
+
+
+// webSite='https://www.apavital.ro/intreruperi-de-apa'
+// //webSite='https://the-internet.herokuapp.com/'
+
+
+// setTimeout(() => {
+//     console.log("Inside setTimeout")
+//     let theString=getText(webSite)
+//     console.log(theString)
+// },
+// 2000
+// )
+
+test()
---
+++ parseString.js
@@ -0,0 +1,7 @@
+// Ar trebui sa parcurgi stringul, găsește < și știi că ești în tag, iei substr până la primul > 
+// și ai atributele, tii minte unde e > și apoi iei conținutul ( mai apoi faci excepția in caz că e />
+
+// Daca vine tot < tagname creiezi un nou tag și îl reții la părinte
+// Daca e string atunci e doar string
+
+
